3cec973ce7a5c377d172749e47e2f84b39a2165d
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-18 17:53:06 +0900

    created default page

---
+++ homepage/src/index.tsx
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
+import reset from "styled-reset";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  body {
+    background-color: whitesmoke;
+    color: black;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+`;
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+root.render(
+    <>
+        <GlobalStyle />
+        <ChakraProvider>
+            <RecoilRoot>
+                <App />
+            </RecoilRoot>
+        </ChakraProvider>
+    </>
+);
