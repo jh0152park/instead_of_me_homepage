6856ab53808f0404d7537401c2f05dca9d027d1c
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-18 20:45:31 +0900

    created error page

--- homepage/src/components/notfound.tsx
+++ homepage/src/components/notfound.tsx
@@ -1,3 +1,40 @@
+import { Center, Heading, VStack } from "@chakra-ui/react";
+import { Helmet } from "react-helmet";
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
-    return <h1>NotFound</h1>;
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Helmet>
+                <title>Not found / Instead of me</title>
+            </Helmet>
+            <Center
+                w="100%"
+                h="100vh"
+                background="linear-gradient(180deg, #70e391, #c9c7c7)"
+            >
+                <VStack>
+                    <Heading fontSize="60px" mt="-200px">
+                        404 ERROR
+                    </Heading>
+                    <Heading>Opps, something went wrong 😭</Heading>
+                    <Heading
+                        mt="100px"
+                        _hover={{
+                            cursor: "pointer",
+                            color: "whitesmoke",
+                            transition: "all 0.2s ease-in-out",
+                        }}
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    >
+                        Go to Home
+                    </Heading>
+                </VStack>
+            </Center>
+        </>
+    );
 }
