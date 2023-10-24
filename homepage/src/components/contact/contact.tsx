6b1b035f5c79307cb9d77cd16d08497e9ce1dbaf
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-24 20:20:23 +0900

    Added email copy function.

--- homepage/src/components/contact/contact.tsx
+++ homepage/src/components/contact/contact.tsx
@@ -1,13 +1,42 @@
-import { Center, HStack, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+    Center,
+    HStack,
+    Heading,
+    Icon,
+    Text,
+    VStack,
+    useToast,
+} from "@chakra-ui/react";
 import { languageState } from "../../global/project_common";
 import { useRecoilValue } from "recoil";
 import LANGUAGE from "../../global/language";
+import { MdContentCopy } from "react-icons/md";
 
 export default function Contact() {
     const currentLanguage = useRecoilValue(languageState);
+    const toast = useToast();
 
     function onClickKakaoPlusChannel() {
         window.open("http://pf.kakao.com/_aSGjG", "Instead of me", "popup=yes");
+    }
+
+    async function onClickCopy() {
+        try {
+            await window.navigator.clipboard.writeText(
+                "insteadofme.yuhyeon@gmail.com"
+            );
+            toast({
+                status: "success",
+                title: LANGUAGE[currentLanguage]["복사가 완료되었습니다."],
+            });
+        } catch (error: any) {
+            toast({
+                status: "error",
+                title: LANGUAGE[currentLanguage]["복사에 실패하였습니다."],
+                description:
+                    LANGUAGE[currentLanguage]["문제가 발생하였습니다."],
+            });
+        }
     }
 
     return (
@@ -35,6 +64,14 @@ export default function Contact() {
                             Business Email:
                         </Text>
                         <Text>insteadofme.yuhyeon@gmail.com</Text>
+                        <Icon
+                            onClick={onClickCopy}
+                            as={MdContentCopy}
+                            color={"green.400"}
+                            _hover={{
+                                cursor: "pointer",
+                            }}
+                        />
                     </HStack>
                     <HStack>
                         <Text fontWeight="bold" color="green.400">
