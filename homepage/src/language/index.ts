86687a05a898edee9334bdd3d23a445a2ee545f8
Yuhyeon0516 <rladbgus0516@naver.com>
2023-10-24 17:32:53 +0900

    Korean and English are defined.

---
+++ homepage/src/language/index.ts
@@ -0,0 +1,113 @@
+interface Ilanguage {
+    "외주, 그 이상의": string;
+    "가치를 만들고 있습니다.": string;
+    "도전을 열망하는 사람들이 모여,": string;
+    "새로운 도전자분들의 시작을 함께합니다.": string;
+    "우리는 글로벌 마켓에서 Smart Device, IoT, 반도체등": string;
+    "다양한 분야에서 1위를 달리는 삼성전자 출신 S/W Engineer 그룹입니다.": string;
+    "우리는 경험과 소통을 중심으로 단순 외주를 뛰어넘어": string;
+    "새로운 도전을 함께 시작하는 비즈니스 파트너가 되는것이 목표입니다.": string;
+    "Global Co-Work경험을 토대로": string;
+    "의뢰인의 국가, 시간을 뛰어넘습니다.": string;
+    "우리는 선택과 집중을 합니다.": string;
+    "양보단 질을, 높은 완성도의 가치를 믿습니다.": string;
+    "CAPA를 초과하는 Workload로 인해": string;
+    "작업의 완성도를 떨어뜨리지 않겠다고 약속 드립니다.": string;
+    "효율적인 프레임워크를 사용해 iOS 및 Android에서 모두 동작하는 고품질의 어플리케이션을 개발하고 제공합니다.": string;
+    "PC 및 Mobile에서 모두 사용가능한 고품질의 Web사이트를 개발하고 제공합니다.": string;
+    "휴먼에러를 야기하는 단순하고 반복적인 업무를 자동화 합니다. 웹크롤링, 웹자동화을 비롯해 핵심적인 업무툴인 엑셀 자동화를 통해 업무의 효율을 대폭 높힐수 있습니다.": string;
+    "제작문의 및 견적상담": string;
+    "견적검토 및 계약진행": string;
+    "의뢰인 자료 전달": string;
+    제작: string;
+    "수정 및 확정": string;
+    제작완료: string;
+    "협업문의는 아래 비즈니스이메일 또는 카카오톡 플러스친구 채널을 통해 연락주세요.": string;
+    "카카오톡 플러스친구 채널:": string;
+}
+
+const language: Ilanguage[] = [
+    {
+        "외주, 그 이상의": "외주, 그 이상의",
+        "가치를 만들고 있습니다.": "가치를 만들고 있습니다.",
+        "도전을 열망하는 사람들이 모여,": "도전을 열망하는 사람들이 모여;",
+        "새로운 도전자분들의 시작을 함께합니다.":
+            "새로운 도전자분들의 시작을 함께합니다.",
+        "우리는 글로벌 마켓에서 Smart Device, IoT, 반도체등":
+            "우리는 글로벌 마켓에서 Smart Device, IoT, 반도체등",
+        "다양한 분야에서 1위를 달리는 삼성전자 출신 S/W Engineer 그룹입니다.":
+            "다양한 분야에서 1위를 달리는 삼성전자 출신 S/W Engineer 그룹입니다.",
+        "우리는 경험과 소통을 중심으로 단순 외주를 뛰어넘어":
+            "우리는 경험과 소통을 중심으로 단순 외주를 뛰어넘어",
+        "새로운 도전을 함께 시작하는 비즈니스 파트너가 되는것이 목표입니다.":
+            "새로운 도전을 함께 시작하는 비즈니스 파트너가 되는것이 목표입니다.",
+        "Global Co-Work경험을 토대로": "Global Co-Work경험을 토대로",
+        "의뢰인의 국가, 시간을 뛰어넘습니다.":
+            "의뢰인의 국가, 시간을 뛰어넘습니다.",
+        "우리는 선택과 집중을 합니다.": "우리는 선택과 집중을 합니다.",
+        "양보단 질을, 높은 완성도의 가치를 믿습니다.":
+            "양보단 질을, 높은 완성도의 가치를 믿습니다.",
+        "CAPA를 초과하는 Workload로 인해": "CAPA를 초과하는 Workload로 인해",
+        "작업의 완성도를 떨어뜨리지 않겠다고 약속 드립니다.":
+            "작업의 완성도를 떨어뜨리지 않겠다고 약속 드립니다.",
+        "효율적인 프레임워크를 사용해 iOS 및 Android에서 모두 동작하는 고품질의 어플리케이션을 개발하고 제공합니다.":
+            "효율적인 프레임워크를 사용해 iOS 및 Android에서 모두 동작하는 고품질의 어플리케이션을 개발하고 제공합니다.",
+        "PC 및 Mobile에서 모두 사용가능한 고품질의 Web사이트를 개발하고 제공합니다.":
+            "PC 및 Mobile에서 모두 사용가능한 고품질의 Web사이트를 개발하고 제공합니다.",
+        "휴먼에러를 야기하는 단순하고 반복적인 업무를 자동화 합니다. 웹크롤링, 웹자동화을 비롯해 핵심적인 업무툴인 엑셀 자동화를 통해 업무의 효율을 대폭 높힐수 있습니다.":
+            "휴먼에러를 야기하는 단순하고 반복적인 업무를 자동화 합니다. 웹크롤링, 웹자동화을 비롯해 핵심적인 업무툴인 엑셀 자동화를 통해 업무의 효율을 대폭 높힐수 있습니다.",
+        "제작문의 및 견적상담": "제작문의 및 견적상담",
+        "견적검토 및 계약진행": "견적검토 및 계약진행",
+        "의뢰인 자료 전달": "의뢰인 자료 전달",
+        제작: "제작",
+        "수정 및 확정": "수정 및 확정",
+        제작완료: "제작완료",
+        "협업문의는 아래 비즈니스이메일 또는 카카오톡 플러스친구 채널을 통해 연락주세요.":
+            "협업문의는 아래 비즈니스이메일 또는 카카오톡 플러스친구 채널을 통해 연락주세요.",
+        "카카오톡 플러스친구 채널:": "카카오톡 플러스친구 채널:",
+    },
+    {
+        "외주, 그 이상의": "We are creating value",
+        "가치를 만들고 있습니다.": "that goes beyond outsourcing.",
+        "도전을 열망하는 사람들이 모여,":
+            "People who are eager for a challenge gather together,",
+        "새로운 도전자분들의 시작을 함께합니다.":
+            "to join new challengers in their beginnings.",
+        "우리는 글로벌 마켓에서 Smart Device, IoT, 반도체등":
+            "We are a group of S/W Engineers from Samsung Electronics who are ranked first in various fields",
+        "다양한 분야에서 1위를 달리는 삼성전자 출신 S/W Engineer 그룹입니다.":
+            "such as smart devices, IoT, and semiconductors in the global market.",
+        "우리는 경험과 소통을 중심으로 단순 외주를 뛰어넘어":
+            "Our goal is to become a business partner that goes beyond simple outsourcing",
+        "새로운 도전을 함께 시작하는 비즈니스 파트너가 되는것이 목표입니다.":
+            "and starts new challenges together, focusing on experience and communication.",
+        "Global Co-Work경험을 토대로":
+            "Based on our Global Co-Work experience,",
+        "의뢰인의 국가, 시간을 뛰어넘습니다.":
+            "we transcend the client's country and time.",
+        "우리는 선택과 집중을 합니다.": "We make choices and focus.",
+        "양보단 질을, 높은 완성도의 가치를 믿습니다.":
+            "We believe in quality over quantity and the value of high perfection.",
+        "CAPA를 초과하는 Workload로 인해":
+            "We promise that we will not degrade work completion",
+        "작업의 완성도를 떨어뜨리지 않겠다고 약속 드립니다.":
+            "due to workload exceeding CAPA.",
+        "효율적인 프레임워크를 사용해 iOS 및 Android에서 모두 동작하는 고품질의 어플리케이션을 개발하고 제공합니다.":
+            "We use efficient frameworks to develop and deliver high-quality applications that run on both iOS and Android.",
+        "PC 및 Mobile에서 모두 사용가능한 고품질의 Web사이트를 개발하고 제공합니다.":
+            "We develop and provide high-quality websites that can be used on both PC and mobile.",
+        "휴먼에러를 야기하는 단순하고 반복적인 업무를 자동화 합니다. 웹크롤링, 웹자동화을 비롯해 핵심적인 업무툴인 엑셀 자동화를 통해 업무의 효율을 대폭 높힐수 있습니다.":
+            "Automates simple and repetitive tasks that cause human errors. Work efficiency can be greatly increased through web crawling, web automation, and Excel automation, a core work tool.",
+        "제작문의 및 견적상담": "Production inquiries and quote consultation",
+        "견적검토 및 계약진행": "Quotation review and contract progress",
+        "의뢰인 자료 전달": "Delivery of client data",
+        제작: "Produce",
+        "수정 및 확정": "Modification and Confirmation",
+        제작완료: "Complete Production",
+        "협업문의는 아래 비즈니스이메일 또는 카카오톡 플러스친구 채널을 통해 연락주세요.":
+            "For collaboration inquiries, please contact us via the business email below or the KakaoTalk Plus Friend channel.",
+        "카카오톡 플러스친구 채널:": "KakaoTalk Plus Friend Channel:",
+    },
+];
+
+export default language;
