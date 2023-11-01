9ebe65d256539a42b8150261e6f3512d0693f171
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-01 21:40:51 +0900

    Applied responsive styles to header.

--- homepage/src/screen/home.tsx
+++ homepage/src/screen/home.tsx
@@ -11,12 +11,12 @@ export default function Home() {
             <Helmet>
                 <title>Home / Instead of me</title>
             </Helmet>
-            <Box id="buffer" w="100%" h="70px"></Box>
+            <Box id="buffer" w="100dvw" h="70px"></Box>
 
-            <AboutUs />
+            {/* <AboutUs />
             <Business />
             <Process />
-            <ContactUS />
+            <ContactUS /> */}
         </>
     );
 }
