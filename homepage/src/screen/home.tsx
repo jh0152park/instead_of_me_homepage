d14ade189e1b37e7ffe0be335f09459535ff22fe
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-01 23:55:42 +0900

    Applied responsive styles to process section.

--- homepage/src/screen/home.tsx
+++ homepage/src/screen/home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
             <AboutUs />
             <Business />
-            {/* <Process /> */}
+            <Process />
             {/* <ContactUS /> */}
         </>
     );
