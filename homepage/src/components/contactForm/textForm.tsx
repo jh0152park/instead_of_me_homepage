bcc5fa3d0a868e28ba84088e97649556b0e1df4e
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-04 22:56:13 +0900

    fixed form text

--- homepage/src/components/contactForm/textForm.tsx
+++ homepage/src/components/contactForm/textForm.tsx
@@ -1,76 +1,77 @@
 import { HStack, Select, Text, Textarea, VStack } from "@chakra-ui/react";
 
 interface ITextForm {
-    select?: boolean;
-    title: string;
-    engTitle: string;
-    name: string;
+	select?: boolean;
+	title: string;
+	engTitle: string;
+	name: string;
 }
 
 export default function TextForm({
-    select = false,
-    title,
-    engTitle,
-    name,
+	select = false,
+	title,
+	engTitle,
+	name,
 }: ITextForm) {
-    return (
-        <HStack
-            alignSelf={"center"}
-            minWidth={{
-                base: "100%",
-                md: "85%",
-                lg: "70%",
-            }}
-            justifyContent={"space-between"}
-            px={"10px"}
-        >
-            <VStack alignItems={"flex-start"}>
-                <Text
-                    color={"black"}
-                    fontWeight={"semibold"}
-                    fontSize={{
-                        base: "10px",
-                        md: "14px",
-                        lg: "18px",
-                    }}
-                >
-                    {title}
-                </Text>
-                <Text
-                    color={"black"}
-                    fontWeight={"semibold"}
-                    fontSize={{
-                        base: "10px",
-                        md: "14px",
-                        lg: "18px",
-                    }}
-                >
-                    {engTitle}
-                </Text>
-            </VStack>
+	return (
+		<HStack
+			alignSelf={"center"}
+			minWidth={{
+				base: "100%",
+				md: "85%",
+				lg: "70%",
+			}}
+			justifyContent={"space-between"}
+			px={"10px"}
+			mb={"50px"}
+		>
+			<VStack alignItems={"flex-start"}>
+				<Text
+					color={"black"}
+					fontWeight={"semibold"}
+					fontSize={{
+						base: "10px",
+						md: "14px",
+						lg: "18px",
+					}}
+				>
+					{title}
+				</Text>
+				<Text
+					color={"black"}
+					fontWeight={"semibold"}
+					fontSize={{
+						base: "10px",
+						md: "14px",
+						lg: "18px",
+					}}
+				>
+					{engTitle}
+				</Text>
+			</VStack>
 
-            {select ? (
-                <Select
-                    maxWidth={{
-                        base: "45%",
-                        md: "50%",
-                        lg: "60%",
-                    }}
-                    name={name}
-                >
-                    <option value={"개인(individual)"}>네(Yes)</option>
-                    <option value={"회사(company)"}>아니오(No)</option>
-                </Select>
-            ) : (
-                <Textarea
-                    maxWidth={{
-                        base: "45%",
-                        md: "50%",
-                        lg: "60%",
-                    }}
-                    name={name}
-                />
-            )}
-        </HStack>
-    );
+			{select ? (
+				<Select
+					maxWidth={{
+						base: "45%",
+						md: "50%",
+						lg: "60%",
+					}}
+					name={name}
+				>
+					<option value={"개인(individual)"}>네(Yes)</option>
+					<option value={"회사(company)"}>아니오(No)</option>
+				</Select>
+			) : (
+				<Textarea
+					maxWidth={{
+						base: "45%",
+						md: "50%",
+						lg: "60%",
+					}}
+					name={name}
+				/>
+			)}
+		</HStack>
+	);
 }
