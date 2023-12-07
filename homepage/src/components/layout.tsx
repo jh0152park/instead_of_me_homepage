0c8de4a04d7e2163983aad921d6ec6e28fbb2a2c
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-07 23:48:10 +0900

    added debug log

--- homepage/src/components/layout.tsx
+++ homepage/src/components/layout.tsx
@@ -4,6 +4,8 @@ import Header from "./header";
 export default function Layout() {
     const { pathname } = useLocation();
 
+    console.log(pathname);
+
     return (
         <>
             {pathname === "/" || "/instead_of_me_homepage" ? null : <Header />}
