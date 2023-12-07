47bb0d4af3eff9bc9b9cc75672d85435a4c3f18c
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-12-07 15:59:14 +0900

    added modal

--- homepage/src/components/instructor/Instruction.tsx
+++ homepage/src/components/instructor/Instruction.tsx
@@ -1,4 +1,18 @@
-import { Box, Heading, VStack, Image } from "@chakra-ui/react";
+import {
+    Box,
+    Heading,
+    VStack,
+    Image,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    Button,
+} from "@chakra-ui/react";
 import { languageState } from "../../global/project_common";
 import { useRecoilValue } from "recoil";
 import LANGUAGE from "../../global/language";
@@ -7,6 +21,7 @@ export default function Instruction() {
     const chractor =
         "https://github.com/jh0152park/instead_of_me_homepage/blob/main/images/emoji/emoji.png?raw=true";
     const currentLanguage = useRecoilValue(languageState);
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <>
@@ -15,26 +30,41 @@ export default function Instruction() {
                 <Heading>{LANGUAGE[currentLanguage]["말씀해주세요"]}</Heading>
             </VStack>
 
-            <VStack>
-                <Box
-                    w="250px"
-                    h="250px"
-                    borderRadius="50%"
-                    border="3px solid"
-                    borderColor="teal.500"
-                    _hover={{
-                        cursor: "pointer",
-                    }}
-                >
-                    <Image
-                        w="100%"
-                        h="100%"
-                        src={chractor}
-                        objectFit={"cover"}
-                        borderRadius="full"
-                    />
-                </Box>
-            </VStack>
+            <Box
+                w="250px"
+                h="250px"
+                borderRadius="50%"
+                border="3px solid"
+                borderColor="teal.500"
+                _hover={{
+                    cursor: "pointer",
+                }}
+                onClick={onOpen}
+            >
+                <Image
+                    w="100%"
+                    h="100%"
+                    src={chractor}
+                    objectFit={"cover"}
+                    borderRadius="full"
+                />
+            </Box>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody></ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant="ghost">Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     );
 }
